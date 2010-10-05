9faf97d5f5e6f7af3749760e5169933fd69b7841
Brian Luft <brian@unbracketed.com>
2010-10-04 21:33:00 -0700

    adds some foundational pseudocode and general overview text

--- snowbird/base.py
+++ snowbird/base.py
@@ -1,3 +1,15 @@
+class Counter(object):
+    """
+    Provides a counter for map objects
+    """
+
+
+class Relation(object):
+    """
+    Represents a relation
+    """
+
+
 class BaseMap(object):
     """
     BaseMap
@@ -15,12 +27,76 @@ class BaseMap(object):
     special handling - making it more difficult to generalize - so the framework
     aims to make it easy to use some common mapping patterns.
     
+    Attributes:
+        
+        
+            
+    
+    class Articles(BaseMap):
+        source = LegacyModel
+        destination = DestinationModel
+        
+        tree_id = Counter()
+        
+        authors = M2M(to=LegacyAuthors, allow_null=False)
+        
+    In this case you'll have
+    a = Articles()
+    a.tree_id
+    1
+    a.tree_id
+    2
+    
     
     """
     
-class BaseSource(object):
-    """
-    A Source is an abstraction of a set of data. This will most commonly be
-    something like a database table, 
-    """
+    _counters = {}
+    model = None
+    legacy_model = None
+    batch_size = 1000
+    _batches = []
+    _insert_sql = ""
+    _select_sql = ""
+    
+    
+    def __init__(self, *args, **kwargs):
+        pass
+    
+    def process_row(self, row):
+        """
+        Process a single row at a time.
+        
+        Data can be returned in a few different formats:
+        
+        1. self.destination()
+        2. As a dict: {'field1': row.field1}
+        3. As a tuple (row.field1, row.field2, )
+        """
+        raise NotImplementedError
+    
+    def process_queryset(self, qs):
+        """
+        Process a queryset
+        """
+        raise NotImplementedError
+    
+    def handle(self):
+        """
+        If you need to manage aspects of the migration that can't be
+        handled by the runner, you can override `handle`. This allows you to
+        handle all aspects of the migration, at the cost of more code to write.
+        
+        """
+        raise NotImplementedError
+    
+    def 
+    
+    
+    
+#class BaseSource(object):
+#    """
+#    A Source is an abstraction of a set of data. This will most commonly be
+#    something like a database table, 
+#    
+#    """
 
