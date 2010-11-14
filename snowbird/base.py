40f69683c0052f50718e04d67f23b4b25fa5b277
Brian Luft <brian@unbracketed.com>
2010-11-13 23:36:47 -0800

    started generalized extraction engine for django primary model

--- snowbird/base.py
+++ snowbird/base.py
@@ -136,38 +136,95 @@ from MySQLdb import OperationalError
 
 E_DIR = '/tmp'
 
-def extract_for_model(model, db='default', fields=[]):
-    """
-    Writes the contents of the database table for a
-    Django model to a .csv file
-    """
+class ModelMap:
 
-    #TODO mysql writes output files with mysql user acl
-    cur = connections[db].cursor()
+    def __init__(self, model,  sources=[], **options):
+        self.model = model
+        self.sources = {}
+        
+        #TODO inspect relations for model
+
+        self.add_source(model)
+        for source in sources:
+            self.add_source(source)
+
+        self.extract_dir = E_DIR
+
+
+    def add_source(self, modelsource):
+        
+        #TODO is this a relation of the primary model?
+        model = modelsource.model
+        self.sources[model] = getattr(modelsource, 'fields', 
+                [f.name for f in model._meta.fields]
+
+    def get_field_col_map(self, model, fields=[])
+        
+        #TODO composite fields will break
+        if not fields::
+        	_fields = dict([(f.name, f.attname, ) \
+        	                      for f in self.model._meta.fields])
+        else:
+        	_fields = dict([(name, 
+        	        model._meta.get_field_by_name(name).attname, ) \
+        	        for name in options['fields'])
+
+
+    def extract(self):
+        "Do an extract operation for all sources"
+        for source in self.sources:
+            extract_for_model(source, self.sources[source])
+
+    def extract_for_model(model, fields=[]):
+        """
+        Writes the contents of the database table for a
+        Django model to a .csv file
+        """
+
+        #TODO mysql writes output files with mysql user acl
+        
+        #TODO get correct cursor for model
+        db = 'default'
+        cur = connections[db].cursor()
+        
+        table = model._meta.db_table
+        if not fields:
     
-    table = model._meta.db_table
-    if not fields:
-    	fields = model._meta.fields
+            colmap = self.get_field_col_map(model, 
+                                        getattr(modelsource, 'fields', []))
+        field_names = [f.attname for f in model._meta.fields]
+        filename = os.path.join(E_DIR, 'e_%s_%s.csv' % (db, table, ))
 
-    field_names = [f.attname for f in model._meta.fields]
-    filename = os.path.join(E_DIR, 'e_%s_%s.csv' % (db, table, ))
+        MYSQL_OUTFILE_TEMPLATE = \
+        """
+        SELECT %s INTO OUTFILE '%s' 
+        FIELDS TERMINATED BY ',' OPTIONALLY ENCLOSED BY '"'
+        LINES TERMINATED BY '\n'
+        FROM %s
+        """ % (','.join(field_names),
+            filename,
+            table, )
+                
+        try:
+            cur.execute(MYSQL_OUTFILE_TEMPLATE)
+        except OperationalError:
+            raise
+        finally:
+            cur.close()
+        return (filename, table, field_names, )
 
-    MYSQL_OUTFILE_TEMPLATE = \
-    """
-    SELECT %s INTO OUTFILE '%s' 
-    FIELDS TERMINATED BY ',' OPTIONALLY ENCLOSED BY '"'
-    LINES TERMINATED BY '\n'
-    FROM %s
-    """ % (','.join(field_names),
-           filename,
-           table, )
-            
-    try:
-    	cur.execute(MYSQL_OUTFILE_TEMPLATE)
-    except OperationalError:
-        raise
-    finally:
-        cur.close()
-    return (filename, table, field_names, )
+    def get_reader(filename):
+        return csv.reader(open(filename, 'r'))
 
+    def get_as_dict(modelsource):
+        _d = {}
+        filename, table, fields = modelsource 
+        for row in get_reader(filename):
+            if len(row) == len(fields):
+                _d.update(row)
+            else:
+                #TODO
+                pass
 
+    def cache(model, key='id'):
+        
