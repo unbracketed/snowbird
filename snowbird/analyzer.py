3cfba4fe76000f8ec327eb9f8e4249b436c726bd
Brian Luft <brian@unbracketed.com>
2010-12-25 13:30:11 -0800

    moved a few bits around

---
+++ snowbird/analyzer.py
@@ -0,0 +1,12 @@
+def get_related_fields(model):
+	pass
+
+def get_table_size(model):
+	pass
+
+def get_row_size(model):
+	pass
+
+#see how south introspects models
+#look at Django DB adapter
+#determine which DB adapter
