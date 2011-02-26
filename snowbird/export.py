737b486e786e4060fd8c83a60a067685d647d1eb
Brian Luft <brian@unbracketed.com>
2011-02-25 22:19:01 -0800

    removes refactored code

--- snowbird/export.py
+++ snowbird/export.py
@@ -6,40 +6,8 @@ class ExportDjango:
     """
 
     def __init__(self, iosrc):
-        
-
         self.extract_dir = E_DIR
 
-
-    #def add_source(self, modelsource):
-        #"Add a data source to the map"
-
-        ##TODO is this a relation of the primary model?
-        #model = modelsource.model
-        #self.sources[model] = getattr(modelsource, 'fields', 
-                #[f.name for f in model._meta.fields])
-
-    #def get_field_col_map(self, model, fields):
-        #"""
-        #Returns a dict mapping each selected field of a 
-        #Django model to its DB column name
-
-            #{model_field_name: db_column_name}
-        #"""
-        ##TODO composite fields will break
-        #if not fields:
-            #_fields = dict([(f.name, f.attname, ) \
-                                  #for f in self.model._meta.fields])
-        #else:
-            #_fields = dict([(name, 
-                    #model._meta.get_field_by_name(name).attname) \
-                    #for name in options['fields']])
-
-
-    #def extract(self):
-        #"Do an extract operation for all sources"
-        #for source in self.sources:
-            #extract_for_model(source, self.sources[source])
 
     def extract_for_model(model, fields=[]):
         """
