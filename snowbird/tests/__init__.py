a26b75f04b37293ec620130f5cc4b915cdf87e2f
Brian Luft <brian@unbracketed.com>
2011-02-26 13:24:45 -0800

    adds some tests for DjangoModel fields/exclude attributes

--- snowbird/tests/__init__.py
+++ snowbird/tests/__init__.py
@@ -2,7 +2,8 @@
 Snowbird Tests
 """
 import unittest
-from snowbird.io import (DjangoModel, ModelMissingError, InvalidModelError)
+from snowbird.io import (DjangoModel, ModelMissingError, InvalidModelError, 
+        InvalidSourceFieldError)
 from snowbird.tests.models import TestModel
 
 
@@ -31,3 +32,43 @@ class TestDjangoModel(unittest.TestCase):
         dm = IOTest()
         self.assertTrue(isinstance(dm, DjangoModel))
         self.assertTrue(issubclass(dm.model, TestModel))
+
+    def test_djangomodel_fields(self):
+        """test that fields can be specified"""
+        class IOTest(DjangoModel):
+            model = TestModel
+            fields = ('field_1', 'field_3', 'field_5')
+        dm = IOTest()
+        self.assertEqual(('field_1', 'field_3', 'field_5'), dm._fields)
+
+    def test_djangomodel_bogus_field(self):
+        """test that a bogus field raises exception"""
+        class IOTest(DjangoModel):
+            model = TestModel
+            fields = ('giggity', )
+        self.assertRaises(InvalidSourceFieldError, IOTest)
+
+    def test_exclude(self):
+        """test that fields can be excluded"""
+        class IOTest(DjangoModel):
+            model = TestModel
+            exclude = ('field_3', 'field_4', 'field_5')
+        dm = IOTest()
+        self.assertEqual(['id', 'field_1', 'field_2'], dm._fields)
+
+    def test_bogus_exclude(self):
+        """test that specifying a bogus exclude field generates 
+        an exception"""
+        class IOTest(DjangoModel):
+            model = TestModel
+            exclude = ('griffin', )
+        self.assertRaises(InvalidSourceFieldError, IOTest)
+
+    def test_fields_and_exclude(self):
+        """test that fields and exclude work together properly"""
+        class IOTest(DjangoModel):
+            model = TestModel
+            fields = ('id', 'field_1', 'field_2', 'field_3')
+            exclude = ('id', )
+        dm = IOTest()
+        self.assertEqual(['field_1', 'field_2', 'field_3'], dm._fields)
