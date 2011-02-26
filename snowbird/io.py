0029115266d6f905326e946aa0d1b8fc98adffff
Brian Luft <brian@unbracketed.com>
2011-02-26 13:24:20 -0800

    improves Exceptions verbosity; cleans up fields/excludes handling

--- snowbird/io.py
+++ snowbird/io.py
@@ -6,23 +6,6 @@ import logging
 LOG = logging.getLogger()
 
 
-class NoSourceFieldsDefinedError(Exception):
-    """Raised when a source cannot determine any fields to query"""
-    def __init__(self, klass):
-        self.msg = "No fields/columns to query for " %(klass.__name__)
-    def __str__(self):
-        return self.msg
-
-
-class InvalidSourceFieldError(Exception):
-    """Raised when a field is declared on the Source that cannot
-    be resolved"""
-    def __init__(self, field, klass):
-        self.msg = "Field %s not found on %s" %(field, klass.__name__)
-    def __str__(self):
-        return self.msg
-
-
 class ModelMissingError(Exception):
     """Raised when the Django model class isn't specified"""
     def __init__(self, klass):
@@ -39,12 +22,30 @@ class InvalidModelError(Exception):
         return self.msg
 
 
+class NoSourceFieldsDefinedError(Exception):
+    """Raised when a source cannot determine any fields to query"""
+    def __init__(self, klass):
+        self.msg = "No fields/columns to query for " %(klass.__name__)
+    def __str__(self):
+        return self.msg
+
+
+class InvalidSourceFieldError(Exception):
+    """Raised when a field is declared on the Source that cannot
+    be resolved"""
+    def __init__(self, fields, klass):
+        self.msg = "Fields %s not found on %s" %(repr(fields), klass.__name__)
+    def __str__(self):
+        return self.msg
+
+
+
+
 class DjangoModel(DBMixin):
     """
     A Django Model interface to a dataset.
     """
     def __init__(self, **options):
-
         if not getattr(self, 'model', False):
             raise ModelMissingError(self)
         elif not issubclass(self.model, models.Model):
@@ -53,14 +54,19 @@ class DjangoModel(DBMixin):
         self._fields = []
 
         if hasattr(self, 'fields'):
-            for field in self.fields:
-                if not field in self.get_default_fields():
-                    raise InvalidSourceFieldError(field, self.model)
+            if not set(self.fields).issubset(set(self.get_default_fields())):
+                raise InvalidSourceFieldError(
+                        set(self.fields)-set(self.get_default_fields()),
+                        self.model)
             self._fields = self.fields
         else:
             self._fields = self.get_default_fields()
 
         if hasattr(self, 'exclude'):
+            if not set(self.exclude).issubset(set(self._fields)):
+                raise InvalidSourceFieldError(
+                        set(self.exclude)-set(self._fields),
+                        self.model)
             self._fields = [f for f in self._fields if not f in self.exclude]
 
         if not self._fields:
