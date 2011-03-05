cf5ea6b422e6297910a2684ff7bb55ab29dc4619
Brian Luft <brian@unbracketed.com>
2011-03-04 20:13:07 -0800

    removes unnecessary settings

--- snowbird/tests/settings.py
+++ snowbird/tests/settings.py
@@ -17,28 +17,7 @@ DATABASES = {
     }
 }
 
-
-#MIDDLEWARE_CLASSES = (
-    #'django.middleware.common.CommonMiddleware',
-    #'django.contrib.sessions.middleware.SessionMiddleware',
-    #'django.middleware.csrf.CsrfViewMiddleware',
-    #'django.contrib.auth.middleware.AuthenticationMiddleware',
-    #'django.contrib.messages.middleware.MessageMiddleware',
-#)
-
-#ROOT_URLCONF = 'dummy.urls'
-
-
 INSTALLED_APPS = (
-    #'django.contrib.auth',
-    #'django.contrib.contenttypes',
-    #'django.contrib.sessions',
-    #'django.contrib.sites',
-    #'django.contrib.messages',
-    # Uncomment the next line to enable the admin:
-    # 'django.contrib.admin',
-    # Uncomment the next line to enable admin documentation:
-    # 'django.contrib.admindocs',
     'django_nose',
     'snowbird.tests',
 )
