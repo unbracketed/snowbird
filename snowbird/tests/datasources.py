efe57eac1d8a509556ec904ca701163c5575b30a
Brian Luft <brian@unbracketed.com>
2011-03-04 20:11:10 -0800

    adds second DB definition to test settings

--- snowbird/tests/datasources.py
+++ snowbird/tests/datasources.py
@@ -1,6 +1,12 @@
+from django.db import connections
 from snowbird.io import DjangoModel
 from snowbird.tests.models import TestModel
 
 
 class TestModelSource(DjangoModel):
     model = TestModel
+
+
+class TestModelSource2(DjangoModel):
+    model = TestModel
+    connection = connections['number_two']
