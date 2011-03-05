3378ffdf0297865ec1244e96ef0915f7538a9d01
Brian Luft <brian@unbracketed.com>
2011-03-04 20:10:52 -0800

    minor cleanups

--- snowbird/db.py
+++ snowbird/db.py
@@ -1,4 +1,7 @@
 import logging
+from time import time
+
+
 LOG = logging.getLogger()
 
 
@@ -27,9 +30,9 @@ class DBMixin(object):
                                                     ','.join(self._fields),
                                                     self.values_placeholder, )
 
-    def execute_queries(self, queries):
-        for query in queries:
-            self.execute_query(query)
+    #def execute_queries(self, queries):
+        #for query in queries:
+            #self.execute_query(query)
 
     def execute_query(self, query):
 
@@ -40,36 +43,35 @@ class DBMixin(object):
         cursor.close()
         return results
 
-
     def insert_values_list(self, query, values):
         """
         Performs an INSERT query using the list of values.
         """
-
         if not values:
             LOG.warning("insert called with no values")
             return
 
+        #TODO get DB from connection
         cursor = self.get_connection().cursor()
         cursor.execute("SET FOREIGN_KEY_CHECKS=0")
+        t0 = time()
         result = cursor.executemany(query, values)
         cursor.connection.commit()
         cursor.close()
+        LOG.info("[OUT]\t%s\t%s" %(self.__class__.__name__, time() - t0))
         return result
-
 
     def insert_dict_list(self, data):
         """
         Insert a list of dicts into this model.
         """
-
         sql = "INSERT INTO %s (%s) VALUES (%s)" \
                 % (self.table,
                     ','.join(self.columns),
                     ','.join(['%s']*len(self.columns)))
 
         inserts = [[row[col] for col in self.columns] for row in data]
-        return self.do_insert(inserts, sql)
+        return self.insert_values_list(sql, inserts)
 
     def get_metrics(self):
         """Returns row count, avg row size"""
@@ -77,4 +79,3 @@ class DBMixin(object):
 
         res = self.execute_query(sql)
         return {'rows': res[0][0]}
-
