c49675d317756b92b372032d1e9f8aef8b71ecff
Brian Luft <brian@unbracketed.com>
2011-07-04 14:55:29 -0700

    started unifying my io stuff into the DataSource code

--- snowbird/datastores.py
+++ snowbird/datastores.py
@@ -1,58 +1,92 @@
+from collections import namedtuple
 from brewery.ds import base
+from django.db import connections
+import django
+
+#from snowbird.io import DjangoModel
+
+Dj_to_Br = namedtuple('Dj_to_Br', 'Field type meta')
+FIELD_TYPE_MAP = (
+        Dj_to_Br(django.db.CharField, "text", "typeless"),
+        )
+
 
 
 class DjangoModelSource(base.DataSource):
-	"""
-	Use a Django Model as a DataSource
-	"""
+    """
+    Use a Django Model as a DataSource
+    """
+
+    def __init__(self, connection, model):
+        self.connection = connections[connection]
+        self.django_model = model
+        self._fields = None
+        _fields = model.get_fields()
+
+    def initialize(self):
+        pass
+
+    def finalize(self):
+        pass
+
+    @property
+    def fields(self):
+        if self._fields:
+            return self._fields
+
+        fields = []
+        for column in self.get_fields():
+            field = base.Field(name=column.name)
+            field.concrete_storage_type = column.type
+
+            for conv in FIELD_TYPE_MAP:
+                if issubclass(column.__class__, conv['Field']):
+                    field.storage_type = conv['type']
+                    field.analytical_type = conv['meta']
+                    break
+
+            if not field.storage_type:
+                field.storaget_tpye = "unknown"
+
+            if not field.analytical_type:
+                field.analytical_type = "unknown"
+
+            fields.append(field)
+
+        self._fields = fields
+
+        return fields
+
+    def read_fields(self):
+        return self.fields
+
+    def rows(self):
+        return self.django_model.get_data()
+
+    def records(self):
+        return self.django_model.objects.all().values()
+
+
+class DjangoModelTarget(base.DataTarget):
+    """
+    Use a Django Model as a DataTarget
+    """
 
     def __init__(self, connection, model):
         self.connection = connections[connection]
         self.model = model
         self._fields = None
-
-    def initialize(self):
-    	pass
-
-    def finalize(self):
-    	pass
+        self.buffer_size = 1000
 
     @property
     def fields(self):
-    	#calculate fields and convert to brewery
-
-    def read_fields(self):
-        #FIXME
-    	self._fields = self.get_fields()
-
-    def rows(self):
-        #FIXME
-    	return self.model.objects.all()
-
-    def records(self):
-    	return self.model.objects.all().values()
-
-
-class DjangoModelTarget(base.DataTarget):
-	"""
-	Use a Django Model as a DataTarget
-	"""
-
-    def __init__(self, connection, model):
-    	self.connection = connections[connection]
-    	self.model = model
-    	self._fields = None
-    	self.buffer_size = 1000
-
-    @property
-    def fields(self):
-    	 pass
+         pass
 
     def initialize(self):
         self._buffer = []
 
     def finalize(self):
-    	pass
+        pass
 
     def append(self, obj):
         if type(obj) == dict:
--- snowbird/main.py
+++ snowbird/main.py
@@ -1,3 +1,7 @@
+from brewery.stream import Stream
+
+
+
 #load the config file
 
 #calculate jobs for the run
@@ -10,4 +14,17 @@
 
 #tasks should be atomic
 #resume can figure out last good task in a run
-#delta can reuse 
+#delta can reuse
+
+
+
+#get the datamaps
+for job in jobs:
+    stream = Stream()
+    for datamap in job.datamaps:
+        if issubclass(datamap.source, DjangoModel):
+            stream.django_model_source(datamap.source)
+            #if related, attach related mapped IDs
+        if issubclass(datamap.destination, DjangoModel):
+        	stream.django_model_target(datamap.destination)
+        stream.run()
